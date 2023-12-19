609677e16e067dcbb58a39f409316fe46634e9bc
justmeet0924 <93417919+justmeet0924@users.noreply.github.com>
2023-12-19 12:04:59 +0900

    Update waterdrop.js

--- waterdrop.js
+++ waterdrop.js
@@ -1,24 +1,22 @@
-function waterdrop(color){
+function waterdrop(x,y,R,color){
   
-  for(let r=0; r<200; r+=10){
+  for(let r=0; r<R; r+=10){
     
-    circledrop(r,color);
+    circledrop(x,y,r,color);
     
   }
   
 }
 
-function circledrop(r,color){
+function circledrop(x,y,r,color){
   
   noStroke();
   let num = 20;
   for(let i=0; i<num; i++){
     fill(color);
     let rad = 2*PI*random();
-    circle(r*sin(rad),r*cos(rad),100/sqrt(r)*random());
+    circle(x+r*sin(rad),y+r*cos(rad),100/sqrt(r)*random());
    
   }
   
 }
-
-
