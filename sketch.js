454cd9ec5b5bbe098b3f80e0f58300392b03c36e
justmeet0924 <93417919+justmeet0924@users.noreply.github.com>
2023-12-19 11:06:44 +0900

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(400, 400);
+
+    noLoop();
+    blendMode(OVERLAY)
+}
+
+function draw() {
+    background(20);
+
+
+    let color = "yellow";
+    push();
+    translate(width / 2, height / 2);
+    waterdrop(color);
+    pop();
+
+
+
+
+}
+
+
+
+function mouseClicked() {
+
+    push();
+    let color = color(250 * random(), 250, 0);
+    translate(mouseX, mouseY);
+    waterdrop(color);
+    pop();
+
+
+}
